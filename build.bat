53f73bed78c91700c420cca676655fda816c5517
KanonZombie <33068818+KanonZombie@users.noreply.github.com>
2018-08-31 14:59:53 -0300

    Bajando diferencias con el bin original (actual 1870)
    Embrollo revisado contra debugger de REGEN y EXODUS

--- build.bat
+++ build.bat
@@ -8,8 +8,13 @@ IF EXIST VBinDiff.exe VBinDiff "X-Men 2 - Clone Wars (W) [!].bin" "x2_reasm.bin"
 
 FC "X-Men 2 - Clone Wars (W) [!].bin" "x2_reasm.bin" > diferencias.txt
 
+ECHO total lineas en diferencias: FC "X-Men 2 - Clone Wars (W) [!].bin" "x2_reasm.bin" | find /c /v ""
+
 REM Expresiones para buscar en diferencias.txt
 
 REM Issue ADD optimizado como ADDI
 REM ([0-9ABCDEF]{8}: D0 06\r\n[0-9ABCDEF]{8}: [0-9ABCDEF][0-9ABCDEF] [0-9ABCDEF][0-9ABCDEF]\r\n)
 REM =============ADDI issue===========\r\n\1=================================\r\n
+
+REM 
+REM [0-9ABCDEF]{8}: FF 00\r\n
