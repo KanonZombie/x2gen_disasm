fa2765d0412325100ee533f9d47b4c35c2b92e30
KanonZombie <33068818+KanonZombie@users.noreply.github.com>
2018-10-25 18:56:16 -0300

    Mas etiquetas

--- offsetsMemoria.asm
+++ offsetsMemoria.asm
@@ -7,24 +7,31 @@
 ; relative to A5 (0x200 in runtime)
 
 ram_offsetNivelActual	    equ -$359C
-ram_offsetNivelMagnetoJoin	equ -$3B7E
+;ram_offsetNivelMagnetoJoin	equ -$3B7E
 ram_offsetVidasActual      	equ -$38A8
-ram_offsetArrayNiveles     	equ -$E52
 
-ram_offsetNivelSiberiaB1Blizzard     	equ -$3B70
+ram_offsetArrayNiveles      	equ -$E52
+ram_offsetArrayNivelesCount   	equ -$E54
 
-ram_offsetNivelSavageC1Jungle_1     	equ -$3B5A
-ram_offsetNivelSavageB1Jungle_2     	equ -$3B5E
-ram_offsetNivelSavageA2Perimeter_1     	equ -$3B60
+ram_offsetNivelBaniMazaA1Moving_Blocks      equ -$3B56
+ram_offsetNivelSavageC1Jungle_1          	equ -$3B5A
+ram_offsetNivelSavageB1Jungle_2          	equ -$3B5E
+ram_offsetNivelSavageA2Perimeter_1         	equ -$3B60
+ram_offsetNivelSpaceB1Maintenance_Shaft     equ -$3B66
+ram_offsetNivelSiberiaB1Blizzard          	equ -$3B70
+ram_offsetNivelSentinelsD1Exterior_1        equ -$3B72
+ram_offsetNivelCerebroScreenSentinels       equ -$3B7A
+
+ram_offsetNivelCerebroScreenAvalon          equ -$3B7C
+ram_offsetNivelCerebroScreenBaniMaza        equ -$3B7E
+ram_offsetNivelCerebroScreenSavage          equ -$3B80
+ram_offsetNivelCerebroScreenSpace           equ -$3B82
+ram_offsetNivelCerebroScreenClone           equ -$3B84
 ram_offsetNivelCerebroScreenCredits     	equ -$3B86
 ram_offsetNivelCerebroScreenPlayerSelect	equ -$3B88
 ram_offsetNivelCerebroScreenLogoAndDemo     equ -$3B8A
 ram_offsetNivelCerebroScreenFailure         equ -$3B8C
-ram_offsetNivelBaniMazaA1Moving_Blocks      equ -$3B56
-ram_offsetNivelCerebroScreenBaniMaza        equ -$3B7E
 ram_offsetNivelCerebroScreenFinalText       equ -$3B8E
-ram_offsetNivelSpaceB1Maintenance_Shaft     equ -$3B66
-ram_offsetNivelCerebroScreenClone           equ -$3B84
 
 ram_offsetLecturaPad_A     	equ -$7A8
 ram_offsetLecturaPad_B     	equ -$7A6
@@ -35,6 +42,8 @@ rom_offsetRutinaLimpiarCRAM	                    equ SubRutinaLimpiarCRAM-$200
 rom_offsetRutinaHabilitarInterrupciones	        equ SubrutinaHabilitarInterrupciones-$200
 rom_offsetRutinaDeshabilitarInterrupciones	    equ SubrutinaDesabilitarInterrupciones-$200
 rom_offsetRutinaSetearValoresIniciales          equ SubrutinaSetearValoresIniciales-$200
+rom_offsetRutinaCargarDataDeNivel               equ SubrutinaCargarDataDeNivel-$200
+rom_offsetRutinaBlanquearRAMNoReservada         equ loc_0000438A-$200
 
 rom_offsetUbicacionArrayBeast                   equ UbicacionArrayBeast-$200-$2
 rom_offsetUbicacionArrayCyclops                 equ UbicacionArrayCyclops-$200-$2
