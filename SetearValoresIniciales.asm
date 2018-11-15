940c6d942b2ac609f7971959e6bd89574c9c1ec7
KanonZombie <33068818+KanonZombie@users.noreply.github.com>
2018-11-15 18:59:55 -0300

    Refactoring compresion

--- SetearValoresIniciales.asm
+++ SetearValoresIniciales.asm
@@ -1,19 +1,25 @@
 SetearValoresIniciales:
 	LINK	A6, #0
-	CLR.w	-$3B94(A5)
+
+	CLR.w	-$3B94(A5)							; C66C
 	MOVE.b	#$FF, -$3BBC(A5)					; Pone FF en la parte alta de C644
+	
 	MOVEQ	#0, D0								; d0 = 0x00000000
-	MOVE.l	D0, -$1758(A5)						
-	CLR.w	-$1754(A5)
-	MOVE.w	#9, ram_offsetVidasActual(A5)		; Inicializa con 9 vidas
-	CLR.w	-$3B96(A5)
+	MOVE.l	D0, -$1758(A5)						; EAA8 = 0x00000000
+	
+	CLR.w	-$1754(A5)							; EAAC
+	MOVE.w	#9, ram_offsetVidasActual(A5)		; Inicializa con 9 vidas (c958)
+	CLR.w	-$3B96(A5)							; C66A
 	MOVE.b	#$FF, -$3BBB(A5)					; Pone FF en la parte baja de C644
-	MOVE.l	D0, -$1848(A5)
-	CLR.w	-$1844(A5)	
-	MOVE.w	#9, -$38AA(A5)
+	MOVE.l	D0, -$1848(A5)						; E9B8
+	CLR.w	-$1844(A5)							; E9BC
+	MOVE.w	#9, -$38AA(A5)						; C956
+
 	MOVEQ	#0, D1
-	MOVE.b	ram_offsetNivelSiberiaB1Blizzard(A5), D1
-	MOVE.w	D1, ram_offsetNivelActual(A5)
-	MOVE.b	#1, -$38C2(A5)
+	MOVE.b	ram_offsetNivelSiberiaB1Blizzard(A5), D1	; setea Sibertia como		(C690 - index en array nivels)
+	MOVE.w	D1, ram_offsetNivelActual(A5)				; nivel actual				(CC64)
+
+	MOVE.b	#1, -$38C2(A5)								; C93E
+
 	UNLK	A6
 	RTS
